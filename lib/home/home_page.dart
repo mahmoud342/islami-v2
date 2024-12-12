64c9df9ffc3475d174bed4e3c11e74060bb9f686
abdelaziz mahmoud <abdelazizmahmoud342@gmail.com>
2024-12-12 20:21:48 +0200

    initial project add empty home screen

---
+++ lib/home/home_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  static const String  routeName ="home_page";
+
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold();
+  }
+}
